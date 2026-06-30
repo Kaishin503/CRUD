8b0a3eb26fc398a063295e8e1a6a70fef87372fe
Kaishin504 <kinrintenseibaku@gmail.com>
2026-06-29 21:38:00 -0300

    changes: imported a few libs to create the create account function, the login function and began working on get current user function.

--- CRUDFUNCTIONS.py
+++ CRUDFUNCTIONS.py
@@ -1,9 +1,53 @@
-from DATABASE import engine,Products,Product_Category,Product_Subcategory,StockLog
+from DATABASE import engine,Products,Product_Category,Product_Subcategory,StockLog,User
 from sqlalchemy.orm import Session
 from datetime import datetime
 import pandas as pd
 from fastapi.responses import StreamingResponse
 import io
+import bcrypt
+import jwt
+from datetime import datetime,timedelta
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_account(name,password):
+    with Session(engine) as session:
+        if len(name) <= 4:
+            raise ValueError("NAME MUST BE ABOVE 4 CHARACTERS")
+        name_validation = session.query(User).filter(User.Username == name).first()
+        if name_validation is not None:
+            raise ValueError("NAME ALREADY TAKEN")
+        if len(password) <= 8:
+            raise ValueError("PASSWORD LENGTH MUST BE ABOVE 8 CHARACTERS")
+        hash = bcrypt.hashpw(password.encode("utf-8"),bcrypt.gensalt())
+        hashed_pw = hash.decode("utf-8")
+        new_user = User(
+            Username = name,
+            Password = hashed_pw
+        )
+        session.add(new_user)
+        session.commit()
+        session.close()
+        return {"MESSAGE":"ACCOUNT CREATED"}
+
+def login(name,password):
+    with Session(engine) as session:
+        user_search = session.query(User).filter(User.Username == name).first()
+        if user_search is None:
+            raise ValueError("USER NOT FOUND")
+        stored_hash = user_search.Password.encode("utf-8")
+        password = password.encode("utf-8")
+        if bcrypt.checkpw(password,stored_hash) == False:
+            raise ValueError("INCORRECT PASSWORD")
+        elif bcrypt.checkpw(password,stored_hash) == True:
+            payload = {
+                "sub":str(user_search.UserID),
+                "exp":datetime.now() + timedelta(minutes=30)
+            }
+            token = jwt.encode(payload,os.getenv("SECRET KEY"),os.getenv("ALGORITHM"))
+            return token
 
 def create_product(name,category,subcategory,price,init_stock):
     with Session(engine) as session:
